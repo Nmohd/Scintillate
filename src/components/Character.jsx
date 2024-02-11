532303470b0628ebaee3d216225c373c7cb84cee
Nmohd <najeebu50@gmail.com>
2024-02-11 15:42:06 +0530

    added vercel.json

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -122,7 +122,7 @@ const Character = () => {
                 ))
               ) : (
                 <Center>
-                  <Spinner />
+                  <Spinner color="red" />
                 </Center>
               )}
             </Grid>
